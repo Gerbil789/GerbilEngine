35618869dd0bd501a423da825f7f7ea6883fb0e3
vojta <vojta.rubes.01@gmail.com>
2025-11-29 16:12:12 +0100

    Update workflow

--- Scripts/build_premake.bat
+++ Scripts/build_premake.bat
@@ -1,16 +1,38 @@
 @echo off
+
+setlocal enabledelayedexpansion
+
+REM Go to repo root (the script is inside /Scripts)
+cd /d "%~dp0.."
+
+echo === Building Premake ===
+
 REM Load VS environment
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 
+REM Move into premake dir
+cd vendor\premake || (echo Premake folder missing & exit /b 1)
+
+
 REM Build bootstrap
-cd ..\vendor\premake
 call bootstrap.bat
+if %errorlevel% neq 0 (
+    echo Bootstrap FAILED
+    exit /b 1
+)
+
+REM Enter bootstrap project directory
+cd build\bootstrap || (echo Missing build/bootstrap folder & exit /b 1)
 
 REM REM Build full premake using MSBuild
 cd build\bootstrap
 msbuild Premake5.sln /t:Premake5 /m /p:Configuration=Release /p:Platform=x64
 
-REM Copy final exe to repo root or vendor/bin
-echo Premake5.exe built successfully
+if %errorlevel% neq 0 (
+    echo Premake build FAILED
+    exit /b 1
+)
 
-PAUSE
+echo === Premake5 built successfully ===
+
+endlocal
